b6dbe51cd45a6553e838996aca2ff41a3b89017c
Karan Goel <karangoel59@gmail.com>
2019-09-07 21:07:32 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductList from 'components/Product/ProductList'
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <li className="list-group-item"> ProductName </li>
+    );
+};
+
+export default Product;
